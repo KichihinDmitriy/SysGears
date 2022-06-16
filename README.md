571db1c2124792fe648061859bb4bd90f71440e2
Dmitriy Kichihin <kichihin.dmitriy1@gmail.com>
2022-06-16 15:28:28 +0300

    created app to convert between distance units

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SysGears
+
+# Parcel template
+
+This project was created with Parcel.
+
+To run the project on your computer, install the project's dependencies in the terminal with the `npm install` command.
+Start development mode by running the `npm start` command.
---
+++ src/index.js
@@ -0,0 +1,72 @@
+// Task 1 App to convert between distance units
+
+const data = {
+    "distance": {"unit": "m", "value": ""},
+    "convert_to": "m"
+};
+
+const units = {
+    "m": 1000,
+    "cm": 10,
+    "in": 25.4,
+    "ft": 304.8,
+    "mm": 1,
+    "yd": 914.4,
+    "km": 1e+6
+};
+
+const selectFrom = document.querySelector('.select-from');
+const selectTo = document.querySelector('.select-to');
+const inputFrom = document.querySelector('.input-from');
+const inputTo = document.querySelector('.input-to');
+
+const unitsArray = Object.keys(units);
+const listOfUnits = unitsArray.reduce((acc, unit) => acc + `<option>${unit}</option>`, '');
+
+selectFrom.innerHTML = listOfUnits;
+selectTo.innerHTML = listOfUnits;
+
+const convert = function () {
+
+    let convertValue = data.distance.value;
+    let convertFrom;
+    let convertTo;
+
+    for (const key in units) {
+
+        if(key === data.distance.unit) {
+            convertFrom = units[key];
+        }
+
+        if(key === data.convert_to) {
+            convertTo = units[key];
+        }
+    };
+
+    const convertResult = convertValue * (convertFrom / convertTo);
+
+    inputTo.value = convertResult.toFixed(2);
+    
+    const dataOutput = {"unit": data.convert_to, "value": convertResult.toFixed(2)};
+    console.log(dataOutput);
+};
+
+inputFrom.addEventListener('input', (e) => {
+    data.distance.value = e.target.value;
+    convert();
+});
+// inputTo.addEventListener('input', (e) => {
+//     data.distance.value = e.target.value;
+//     convert();
+// });
+selectFrom.addEventListener('change', (e) => {
+    data.distance.unit = e.target.value;
+    convert();
+});
+selectTo.addEventListener('change', (e) => {
+    data.convert_to = e.target.value;
+    convert();
+});
+
+// Task 2 App for sorting and selecting data
+
