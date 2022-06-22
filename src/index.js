01aa66b719a8e4d2f52f8c7f011622ed6d637305
Dmitriy Kichihin <kichihin.dmitriy1@gmail.com>
2022-06-22 22:14:43 +0300

    created programmable questionnaire

--- src/index.js
+++ src/index.js
@@ -76,10 +76,10 @@ const jsonRequest = {
     //     {"name": "John", "email": "john1@mail.com"},
     //     {"name": "Jane", "email": "jane@mail.com"}]
 
-    "data": [
-        {"user": "mike@mail.com", "rating": 20, "disabled": false},
-        {"user": "greg@mail.com", "rating": 14, "disabled": false},
-        {"user": "john@mail.com", "rating": 25, "disabled": true}]
+    // "data": [
+    //     {"user": "mike@mail.com", "rating": 20, "disabled": false},
+    //     {"user": "greg@mail.com", "rating": 14, "disabled": false},
+    //     {"user": "john@mail.com", "rating": 25, "disabled": true}]
 };
 
 const sortCondition = {
@@ -87,9 +87,9 @@ const sortCondition = {
     //     "include": [{"name": "John"}],
     //     "sort_by": ["email"]}
 
-    "condition": {
-        "exclude": [{"disabled": true}],
-        "sort_by": ["rating"]}
+    // "condition": {
+    //     "exclude": [{"disabled": true}],
+    //     "sort_by": ["rating"]}
 };
 
 const selectSortDataFn = function (jsonRequest, sortCondition) {
@@ -119,7 +119,51 @@ const selectSortDataFn = function (jsonRequest, sortCondition) {
     console.log(resultJSON);
 };
 
-selectSortDataFn(jsonRequest, sortCondition);
+// selectSortDataFn(jsonRequest, sortCondition);
 
 // Task 3 Programmable questionnaire
 
+const dataInput = [
+    {"What is your marital status?": ["Single", "Married"]},
+    {"Are you planning on getting married next year?": ["Yes", "No"]},
+    {"How long have you been married?": ["Less than a year", "More than a year"]},
+    {"Have you celebrated your one year anniversary?": ["Yes", "No"]}
+];
+
+const question = document.querySelector('.question');
+const answerFirst = document.querySelector('.answer-first');
+const answerSecond = document.querySelector('.answer-second');
+const container = document.querySelector('.container-2');
+
+let step = 0;
+
+const questionnaire = function (dataInput) {
+
+    if (step >= dataInput.length) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        };
+        container.innerHTML = '<h2 class="no-question">Sorry no more questions</h2>';
+    } else {
+        question.innerHTML = Object.keys(dataInput[step]);
+        answerFirst.innerHTML = Object.values(dataInput[step]).flat()[0];
+        answerSecond.innerHTML = Object.values(dataInput[step]).flat()[1];
+    }
+
+};
+
+questionnaire(dataInput);
+
+const answerClickFn = function (e) {
+
+    if (e.target.dataset.answer == 0) {
+        step += 1;
+    } else if (e.target.dataset.answer == 1) {
+        step += 2;
+    }
+
+    questionnaire(dataInput);
+};
+
+answerFirst.addEventListener('click', answerClickFn);
+answerSecond.addEventListener('click', answerClickFn);
